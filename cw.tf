953f054dd21dc8b1023d978a3bdb762d3d0701c6
joebui <dienbuijoe@outlook.com>
2024-07-14 12:12:42 +0700

    update cw log groups

--- cw.tf
+++ cw.tf
@@ -1,4 +1,9 @@
 resource "aws_cloudwatch_log_group" "cw_agent" {
-  name              = "/ecs/${local.cluster_name}-cw-agent"
+  name              = "/ecs/${local.cluster_name}/cw-agent"
+  retention_in_days = 1
+}
+
+resource "aws_cloudwatch_log_group" "container_insights_perf" {
+  name              = "/aws/ecs/containerinsights/${local.cluster_name}/performance"
   retention_in_days = 1
 }
